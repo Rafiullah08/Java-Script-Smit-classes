8b44630c056252748430c5403d290631b388ce57
Rafiullah08 <140521887+Rafiullah08@users.noreply.github.com>
2023-12-01 02:09:52 +0500

    Ist initial

---
+++ jv/app.js
@@ -0,0 +1,241 @@
+// alert("hi")
+
+// var age = 23
+// if(age === 23){
+// console.log("welcome")
+// }
+
+
+
+// var userAge = prompt("enter your age")
+
+// if(userAge >= 20){
+// console.log("you are able to admission")
+// } else{
+// console.log("you are not able to admission")
+
+// }
+
+// var userInput = prompt("enter your favorite number")
+
+// if(userInput % 2 === 1){
+// console.log("this is an even number")
+// } else{
+// console.log("this is an odd number")
+
+// }
+
+
+// var prsnt = +prompt("type your percentage")
+// var grade;
+
+// if(prsnt <= 0 && prsnt >= 100 ){
+
+// if(prsnt <= 90){
+// grade = "A+"
+// }
+// console.log(grade)
+
+// }
+// // console.log(prsnt)
+
+
+
+// var num = prompt("type your percantage")
+// // var grade;
+// if(num >=0 && num <= 100){
+// if(num >= 90){
+// grade = "A+"
+// }else if(num >=70){
+//     grade = "B"
+// } else if(num >=50){
+//     grade = "c"
+// } else if(num >=40){
+//     grade = "d"
+// } else if(num <40){
+//     grade = "fail"
+// } 
+
+// }else{
+//     grade = "type write number"
+// }
+
+// console.log(grade)
+
+// collection of date called is ARRAY....
+var months = ["jan", "feb","march","apr"]
+// months.push("asifaa","anwar")
+// months.pop()
+// // months.pop()
+// months.unshift(3)
+// months.shift()
+// months.splice(1,1,"arubaaa","yraaaa")
+// months.splice(2,1)
+// months.slice(0,1)
+
+
+
+
+// console.log(months)
+// console.log(months.slice(2,4))
+
+
+
+// for(var i = 1 ; i <= 9; i++ ){
+// console.log("hello world"+ i)
+// };
+
+
+// for (var i = 1 ; i <= 10 ;i++){
+// console.log(5 + "* " + i + "=" + i*5)
+// }
+
+// for ( var i = 1; i <= 20; i++){
+//     console.log(10 + "*" + i + "=" + i *10)
+// }
+
+
+// var table = prompt("enter a number.");
+// for(var i = 1; i <= 12; i ++){
+//     document.write(table+ " " + "*"+ i + "="+ table*i+ "<br>")
+// }
+
+
+// var userTab = prompt("enter your number.")
+// for (var i= 1; i <= 100; i++){
+//     document.write(userTab + "*" + i + " ="+ userTab *i+ "<br>")
+// }
+
+
+
+// var cities = ["peshawer", "lahore","multan","karachi"]
+// var city = prompt("enter your city name")
+// var ismatch = false
+// for(var i = 0; i <cities.length; i++){
+//     if(city === cities[i]){
+//         document.write("found"+ city)
+//         ismatch = true
+//     } 
+// }
+
+// if (!ismatch) {
+//     console.log("not found"+ city)
+
+// }
+
+
+// var fruits = ["apple", "mango", "orange", "grapes"]
+// var userFruits = prompt("enetr your favorite fruits.")
+// var ismatch = false
+// for(var i = 0; i <= fruits.length; i++){
+//     if(userFruits == fruits[i]){
+//         console.log("userfruits found: "+ userFruits)
+//     ismatch = true
+
+//     }
+// }
+
+// if (!ismatch){
+//     console.log("userfruits not found: "+ userFruits)
+    
+// }
+
+
+// var array = [["apple","grapes", "orange"],["banana","mango","ananas"]]
+// // console.log(array[0][0])
+// for(var i = 0; i<= array.length; i++){
+// for(var j = 0; j <= array.length[i]; i++){
+// console.log(array[i])
+// }
+// }
+
+
+
+// for (var i = 100; i > 0; i-- ){
+//     console.log(i)
+// }
+// for (var i = 1; i <= 50; i++ ){
+//     console.log(i)
+// }
+
+
+
+
+// var a = window.location.href
+// console.log(a)
+
+
+
+
+function Obj(first,last){
+var first = first;
+var last = last
+}
+
+ var fullName = Obj(rafi,ullah)
+ console.log(fullName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
